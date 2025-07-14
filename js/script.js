fed695277f222edd78a76e729a47c7daf91256c1
fabioferrari00 <fabioferrari.ff00@gmail.com>
2025-07-14 13:01:57 +0200

    [add] countdown and random numbers in html

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ const instructions = document.getElementById('instructions');
 const button = document.querySelector('button');
 
 //creo una funzione che genera 5 numeri random compresi da N a N
-const generateRandomNumbers = (min, max) => {
+const genrateRandomNumbers = (min, max) => {
   const random = [];
 
   while (random.length < 5) {
@@ -19,3 +19,18 @@ const generateRandomNumbers = (min, max) => {
   }
   return random;
 }
+
+//assegno il risultato della funzione ad una variabile
+const numbers = genrateRandomNumbers(1, 50);
+
+//ciclo l'array dei numeri per inserirli nell'HTML
+for (let i = 0; i < numbers.length; i++) {
+  numbersList.innerHTML += `<li class="list-item">${numbers[i]}</li>`;
+}
+
+//mi creo una variabile per il countdown
+let time = 10;
+
+//mostro il valore di time all'interno di countdown
+countdownElement.innerText = time;
+
